2859febd3e7046c5cce8bdfff948fed3a99b25a6
Zosu <56417732+Zosu@users.noreply.github.com>
2021-06-08 00:46:04 -0400

    Basic media control bot

--- media_control_bot.py
+++ media_control_bot.py
@@ -1,21 +1,27 @@
 from discord.ext import commands
 import win32api
-from win32con import VK_MEDIA_PLAY_PAUSE, KEYEVENTF_EXTENDEDKEY, VK_MEDIA_NEXT_TRACK, VK_MEDIA_PREV_TRACK
+from win32con import VK_MEDIA_PLAY_PAUSE, KEYEVENTF_EXTENDEDKEY, VK_MEDIA_NEXT_TRACK, VK_MEDIA_PREV_TRACK, VK_LEFT, VK_RIGHT
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='$')
 
 
-@bot.command(name="media", help = "Control the media that the host is streaming")
+@bot.command(name="media", help = "Control the media that the host is streaming with playt, next, previous, jumpforward, and jumpback")
 async def media(ctx, media_action: str):
     if media_action == 'playt':
         win32api.keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_EXTENDEDKEY, 0)
-        await ctx.send("play/pause")
+        await ctx.send(str(ctx.author) + " called for play/pause")
     elif media_action == 'next':
         win32api.keybd_event(VK_MEDIA_NEXT_TRACK, 0, KEYEVENTF_EXTENDEDKEY, 0)
-        await ctx.send("next")
-    elif media_action == 'back':
+        await ctx.send(str(ctx.author) + " called to go to the next")
+    elif media_action == 'previous':
         win32api.keybd_event(VK_MEDIA_PREV_TRACK, 0, KEYEVENTF_EXTENDEDKEY, 0)
-        await ctx.sent("back")
+        await ctx.send(str(ctx.author) + " called to go to the previous ")
+    elif media_action == 'jumpforward':
+        win32api.keybd_event(VK_RIGHT, 0, KEYEVENTF_EXTENDEDKEY, 0)
+        await ctx.send(str(ctx.author) + " called to jump forward")
+    elif media_action == 'jumpback':
+        win32api.keybd_event(VK_LEFT, 0, KEYEVENTF_EXTENDEDKEY, 0)
+        await ctx.send(str(ctx.author) + " called to jump back")
     else:
         await ctx.send("Unsupported input")
 
