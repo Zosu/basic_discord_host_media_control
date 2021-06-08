cc8a3d5c2f62ad2d7fcedf8eff4980cb4e4a84fb
Zosu <56417732+Zosu@users.noreply.github.com>
2021-06-08 01:31:38 -0400

    shortened commands

--- media_control_bot.py
+++ media_control_bot.py
@@ -2,28 +2,39 @@ from discord.ext import commands
 import win32api
 from win32con import VK_MEDIA_PLAY_PAUSE, KEYEVENTF_EXTENDEDKEY, VK_MEDIA_NEXT_TRACK, VK_MEDIA_PREV_TRACK, VK_LEFT, VK_RIGHT
 
-bot = commands.Bot(command_prefix='$')
+#set desired command prefix here
+command_pfx = '!'
 
+bot = commands.Bot(command_prefix=command_pfx)
 
-@bot.command(name="media", help = "Control the media that the host is streaming with playt, next, previous, jumpforward, and jumpback")
-async def media(ctx, media_action: str):
-    if media_action == 'playt':
-        win32api.keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_EXTENDEDKEY, 0)
-        await ctx.send(str(ctx.author) + " called for play/pause")
-    elif media_action == 'next':
-        win32api.keybd_event(VK_MEDIA_NEXT_TRACK, 0, KEYEVENTF_EXTENDEDKEY, 0)
-        await ctx.send(str(ctx.author) + " called to go to the next")
-    elif media_action == 'previous':
-        win32api.keybd_event(VK_MEDIA_PREV_TRACK, 0, KEYEVENTF_EXTENDEDKEY, 0)
-        await ctx.send(str(ctx.author) + " called to go to the previous ")
-    elif media_action == 'jumpforward':
-        win32api.keybd_event(VK_RIGHT, 0, KEYEVENTF_EXTENDEDKEY, 0)
-        await ctx.send(str(ctx.author) + " called to jump forward")
-    elif media_action == 'jumpback':
-        win32api.keybd_event(VK_LEFT, 0, KEYEVENTF_EXTENDEDKEY, 0)
-        await ctx.send(str(ctx.author) + " called to jump back")
-    else:
-        await ctx.send("Unsupported input")
+@bot.command(name = "media", help = "Use commands playt (play toggle), next, prev, jump, or jumpb (jump back) to control host media")
+async def media (ctx):
+    await ctx.send("Use " + command_pfx + " with playt (play/pause), next, prev, jump, or jumpb (jump back) to control host media")
+
+@bot.command()
+async def playt(ctx):
+    win32api.keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_EXTENDEDKEY, 0)
+    await ctx.send(str(ctx.author) + " called for play/pause")
+
+@bot.command()
+async def next(ctx):
+    win32api.keybd_event(VK_MEDIA_NEXT_TRACK, 0, KEYEVENTF_EXTENDEDKEY, 0)
+    await ctx.send(str(ctx.author) + " called to go to the next")
+
+@bot.command()
+async def prev(Ctx):
+    win32api.keybd_event(VK_MEDIA_PREV_TRACK, 0, KEYEVENTF_EXTENDEDKEY, 0)
+    await ctx.send(str(ctx.author) + " called to go to the previous ")
+
+@bot.command()
+async def jump(ctx):
+    win32api.keybd_event(VK_RIGHT, 0, KEYEVENTF_EXTENDEDKEY, 0)
+    await ctx.send(str(ctx.author) + " called to jump forward")
+
+@bot.command()
+async def jumpb(ctx):
+    win32api.keybd_event(VK_LEFT, 0, KEYEVENTF_EXTENDEDKEY, 0)
+    await ctx.send(str(ctx.author) + " called to jump back")
 
 with open("BOT_TOKEN.txt", "r") as token_file:
     TOKEN = token_file.read()
